6594ec19450021af16658d9631c8efabe4814342
Andrea G <muffo87@gmail.com>
2016-12-27 18:32:29 +0000

    Add ctags to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ set -u
 cd ~
 ln -s dotfiles/zshrc ~/.zshrc
 ln -s dotfiles/tmux.conf ~/.tmux.conf
-
 ln -s dotfiles/ackrc ~/.ackrc
 ln -s dotfiles/vimrc ~/.vimrc
 ln -s dotfiles/gitconfig ~/.giconfig
+ln -s dotfiles/ctags ~/.ctags
